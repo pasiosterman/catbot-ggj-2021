ba615e807833cb33b3f0070c3d67b4fae1fac1bf
PasiOsterman <Pasi.osterman@tutanota.com>
2021-01-29 21:32:31 +0200

    Initial commit for unity files

---
+++ Assets/scripts/TestScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ggj2021
+{
+    public class TestScript : MonoBehaviour
+    {
+        public float speed = 10;
+
+        private void Update()
+        {
+            transform.Rotate(0, speed * Time.deltaTime, 0);
+        }
+    }
+}
